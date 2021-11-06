f64f41bf6828011799cda2df48d65777d0ba50e3
Gustavo Antonio Lutz de Matos <gustavo.almatos@gmail.com>
2021-11-06 17:47:29 -0300

    Basic static components

---
+++ frontend/src/components/Footer/index.tsx
@@ -0,0 +1,16 @@
+const Footer = () => {
+    return (
+        <footer className="footer mt-auto py-3 bg-dark">
+            <div className="container">
+                <p className="text-light">App desenvolvido por <a href="https://www.linkedin.com/in/gustavo-antonio-lutz-de-matos/" target="_blank"
+                                                                  rel="noreferrer">Gustavo Antonio Lutz de Matos</a></p>
+                <p className="text-light"><small><strong>Semana Spring React</strong><br/>
+                    Evento promovido pela escola DevSuperior: <a href="https://instagram.com/devsuperior.ig"
+                                                                 target="_blank"
+                                                                 rel="noreferrer">@devsuperior.ig</a></small></p>
+            </div>
+        </footer>
+    );
+}
+
+export default Footer;
