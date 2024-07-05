69bc40cbde64dc96600d4a4e3b2e8f32604e9028
Gustavo Antonio Lutz de Matos <gustavo.almatos@gmail.com>
2024-07-05 10:48:47 -0400

    refactor(properties): refatorado arquivos de properties, convertendo para formato YAML

--- backend/src/main/java/com/devsuperior/dsvendas/DSVendasApplication.java
+++ backend/src/main/java/com/devsuperior/dsvendas/DSVendasApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DsvendasApplication {
+public class DSVendasApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DsvendasApplication.class, args);
+		SpringApplication.run(DSVendasApplication.class, args);
 	}
 
 }
